80bd6d69625180d72a4ca64c05d4685bf1f2fabc
Marcos Felipe <109048913+MarcosF0002@users.noreply.github.com>
2025-04-28 15:37:00 -0300

    Atualiza conexão para usar st.secrets

--- conn.py
+++ conn.py
@@ -1,14 +1,19 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+import json
 
 def get_client():
-    filename = "cafedamanha-71e815f25d5f.json"
     scopes = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive",
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        filename=filename,
+
+    # Pega o dicionário dos secrets
+    service_account_info = st.secrets["gcp_service_account"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        dict(service_account_info),
         scopes=scopes
     )
     return gspread.authorize(creds)
